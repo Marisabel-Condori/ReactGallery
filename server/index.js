dc0e8c601e77f0ff82e00eb053838dbafe2128e9
mari <mari.17ymy@gmail.com>
2022-08-03 01:03:12 -0400

    archivo temporal donde se subiran las imagenes luego ira a la nube

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 import express  from "express";
+import fileUpload from "express-fileupload";
 
 import indexRoutes from './routes/indexRoutes'
 
+
 const app = express()
 
+//hacer correr en el local host 4000
 app.set('port', process.env.PORT || 4000);
 //app.get('/', (req, res) => res.send('hello world'))
+app.use(fileUpload({
+    tempFileDir: '/temp'
+}))
 app.use(indexRoutes)
 
 app.listen(app.get('port'))
