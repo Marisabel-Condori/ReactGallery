5f2856829f75626645a96e96fc901a7db19035f4
mari <mari.17ymy@gmail.com>
2022-08-03 00:44:29 -0400

    hello world

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 import express  from "express";
 const app = express()
 
-app.set('port', process.env.PORT || 4000)
+app.set('port', process.env.PORT || 4000);
+app.get('/', (req, res) => res.send('hello world'))
+app.listen(app.get('port'))
+console.log('Server on port: ', app.get('port'))
