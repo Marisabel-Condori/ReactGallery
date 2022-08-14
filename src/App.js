af732cde344bb23eba8db99776929b5998d8e08d
mari <mari.17ymy@gmail.com>
2022-08-14 13:08:54 -0400

    enrutadores, navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-import './App.css';
+import ImageForm from './pages/ImageForm';
+import ImageDetail from './pages/ImageDetail';
+import ImageGallery from './pages/ImageGallery';
+ 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <h3>hola mundo</h3>
-    </div>
+    // <Router> // ya lo estoy encerrando en BrouseRouter en el index
+      <div className='bg-dark text-light'>
+        <Navbar/>
+        <Routes> 
+        <Route path='/upload' element={<ImageForm/>}/>
+        <Route path='/images/:id' element={<ImageDetail/>}/>
+        <Route path='/' element={<ImageGallery/>}/>
+      </Routes>
+      </div>
+    // </Router>
   );
+  
 }
 
 export default App;
